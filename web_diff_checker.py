06a88a0eaeaf68366f26864a2a1da3a89aad5ec0
llamafarmer <joshsisto@gmail.com>
2019-02-20 21:22:15 -0800

    Check for website differences

---
+++ web_diff_checker.py
@@ -0,0 +1,31 @@
+# Import requests (to download the page)
+import requests
+
+# Import BeautifulSoup (to parse what we download)
+from bs4 import BeautifulSoup
+
+# Import Time (to add a delay between the times the scape runs)
+import time
+
+# Import smtplib (to allow us to email)
+import smtplib
+
+# This is a pretty simple script. The script downloads the homepage of VentureBeat, and if it finds some text, emails me.
+# If it does not find some text, it waits 60 seconds and downloads the homepage again.
+
+# while this is true (it is true by default),
+def check_site_change():
+    # set the url as VentureBeat,
+    url = "http://resume.joshsisto.com"
+    # set the headers like we are a browser,
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko)'
+                             ' Chrome/72.0.3626.109 Safari/537.36'}
+    # download the homepage
+    response = requests.get(url, headers=headers)
+    # parse the downloaded homepage and grab all text, then,
+    # soup = BeautifulSoup(response.text, "lxml")
+    # print(response.text)
+    with open('log_2.txt', 'w') as f:
+        print("log_2.txt", response.text, file=f)
+
+check_site_change()
