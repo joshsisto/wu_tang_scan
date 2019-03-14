2e402b2c21f445ac42d0514ca7f20bee8d4c23ed
Josh Sisto <joshsisto@gmail.com>
2019-03-14 07:39:28 -0700

    created config. Going to use config and wdc together to scan for differences. Might introduce a few more files

--- config.py
+++ config.py
@@ -1,20 +1,58 @@
 
 import os
+import sys
+import time
+import datetime
+
 
 try:
     OUTPUT_DIR = os.path.abspath(os.getenv('OUTPUT_DIR'))
-    print(OUTPUT_DIR)
+    # print(OUTPUT_DIR)
 except Exception as e:
     OUTPUT_DIR = None
-    print(f'Exception: {e}')
+    # print(f'Exception: {e}')
 
 REPO_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
-print(f'Repo DIR {REPO_DIR}')
+# print(f'Repo DIR {REPO_DIR}')
 if not OUTPUT_DIR:
     OUTPUT_DIR = os.path.join(REPO_DIR, 'output')
-    print(f'Output DIR {OUTPUT_DIR}')
+    # print(f'Output DIR {OUTPUT_DIR}')
 
 ARCHIVE_DIR_NAME = 'archive'
 SOURCES_DIR_NAME = 'sources'
 ARCHIVE_DIR = os.path.join(OUTPUT_DIR, ARCHIVE_DIR_NAME)
 SOURCES_DIR = os.path.join(OUTPUT_DIR, SOURCES_DIR_NAME)
+
+# print(f'Output DIR {OUTPUT_DIR}')
+# print(f'Repo DIR {REPO_DIR}')
+# print(f'Archive DIR {ARCHIVE_DIR}')
+# print(f'Sources DIR {SOURCES_DIR}')
+
+
+def get_platform():
+    platforms = {
+        'linux1': 'Linux',
+        'linux2': 'Linux',
+        'darwin': 'OS X',
+        'win32': 'Windows'
+    }
+    if sys.platform not in platforms:
+        # print(f'Platform: {sys.platform}')
+        return sys.platform
+
+    # print(f'Platform: {platforms[sys.platform]}')
+    return platforms[sys.platform]
+
+
+platform = get_platform()
+if platform == 'OS X' or 'Linux':
+    logs_dir = './logs/'
+if platform == 'Windows':
+    logs_dir = '.\\logs\\'
+
+
+def get_tstamp():
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H-%M-%S')
+    return st
+
