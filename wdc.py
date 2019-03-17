ad1bee2f2605a7393f00fc2690043137cc3209ae
Josh Sisto <joshsisto@gmail.com>
2019-03-16 21:19:23 -0700

    Changed scanning and saving

--- wdc.py
+++ wdc.py
@@ -1,6 +1,6 @@
 
-from web_runner import download_url, scan_output, the_differ
-
+from web_runner import download_url
+from scan_and_compare import the_differ, scan_output
 
 def main(url):
     download_url(url)
