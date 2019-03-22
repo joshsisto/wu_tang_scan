7032d434ff2aa31a2c0b09f4b504af60294c4257
Josh Sisto <joshsisto@gmail.com>
2019-03-22 11:24:45 -0700

    Updated file to change directories. This fixed relative path issue.

--- run_wdc.cmd
+++ run_wdc.cmd
@@ -3,5 +3,11 @@
 :: Fetch param1
 set "param1=%~1"
 
+:: Change directories to wdc. This helps with relative path
+cd wdc
+
 :: Execute python with argument
-py -3 ./wdc/wdc.py %param1%
+py -3 wdc.py %param1%
+
+:: Go back where ya came from
+cd ..
